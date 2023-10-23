fc0dd424f14e30725e8049f84da5d3a2a6e76a4b
jurmaev <smartemii03@gmail.com>
2023-10-23 21:04:25 +0500

    add dates

--- src/components/tabs/reviews-tab.tsx
+++ src/components/tabs/reviews-tab.tsx
@@ -4,7 +4,18 @@ type ReviewsTabProps = {
   reviews: ReviewBase[];
 }
 
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Nobember', 'December'];
+
 export default function ReviewsTab({ reviews }: ReviewsTabProps) {
+  function getDateWithDashes(dateString: string) {
+    const date = new Date(dateString);
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
+
+  function getDateWithComma(dateString: string) {
+    const date = new Date(dateString);
+    return `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+  }
   return (
     <div className="film-card__reviews film-card__row">
       <div className="film-card__reviews-col">
@@ -15,7 +26,7 @@ export default function ReviewsTab({ reviews }: ReviewsTabProps) {
 
               <footer className="review__details">
                 <cite className="review__author">{review.user}</cite>
-                <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
+                <time className="review__date" dateTime={getDateWithDashes(review.date)}>{getDateWithComma(review.date)}</time>
               </footer>
             </blockquote>
 
