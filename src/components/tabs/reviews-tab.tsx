61c2faec34959688635009cd8bec812c0c814f61
jurmaev <smartemii03@gmail.com>
2023-10-30 21:22:08 +0500

    render reviews in columns

--- src/components/tabs/reviews-tab.tsx
+++ src/components/tabs/reviews-tab.tsx
@@ -30,29 +30,44 @@ function getDateWithComma(dateString: string) {
 }
 
 export default function ReviewsTab({ reviews }: ReviewsTabProps) {
+  function sliceReviews(start: number, end: number) {
+    return reviews.slice(start, end).map((review) => (
+      <div key={review.id} className="review">
+        <blockquote className="review__quote">
+          <p className="review__text">{review.comment}</p>
+
+          <footer className="review__details">
+            <cite className="review__author">{review.user}</cite>
+            <time
+              className="review__date"
+              dateTime={getDateWithDashes(review.date)}
+            >
+              {getDateWithComma(review.date)}
+            </time>
+          </footer>
+        </blockquote>
+
+        <div className="review__rating">{review.rating}</div>
+      </div>
+    ));
+  }
+
   return (
     <div className="film-card__reviews film-card__row">
-      <div className="film-card__reviews-col">
-        {reviews.map((review) => (
-          <div key={review.id} className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">{review.comment}</p>
-
-              <footer className="review__details">
-                <cite className="review__author">{review.user}</cite>
-                <time
-                  className="review__date"
-                  dateTime={getDateWithDashes(review.date)}
-                >
-                  {getDateWithComma(review.date)}
-                </time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">{review.rating}</div>
+      {reviews.length > 1 ? (
+        <>
+          <div className="film-card__reviews-col">
+            {sliceReviews(0, reviews.length / 2 + 1)}
           </div>
-        ))}
-      </div>
+          <div className="film-card__reviews-col">
+            {sliceReviews(reviews.length / 2 + 1, reviews.length)}
+          </div>
+        </>
+      ) : (
+        <div className="film-card__reviews-col">
+          {sliceReviews(0, reviews.length)}
+        </div>
+      )}
     </div>
   );
 }
