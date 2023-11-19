f3f080db8179ed36acd5e16b374d9c2925ac96b4
jurmaev <smartemii03@gmail.com>
2023-11-19 15:40:16 +0500

    remove actions

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,33 +1,4 @@
 import { createAction } from '@reduxjs/toolkit';
-import {
-  AppRoutesValues,
-  AuthorizationStatusValues,
-  GenresValues,
-} from '../const';
-import { Movie, MoviePreviews, PromoMovie } from '../types/movies';
-import { Reviews } from '../types/reviews';
-
-export const changeGenre = createAction<GenresValues>('genre/change');
-
-export const loadMovies = createAction<MoviePreviews>('movies/load');
-
-export const setSimilarMovies =
-  createAction<MoviePreviews>('movies/getSimilar');
-
-export const setDataFetchingStatus = createAction<boolean>(
-  'data/fetchingStatus'
-);
-
-export const setAuthorizationStatus = createAction<AuthorizationStatusValues>(
-  'authorizationStatus/set'
-);
+import { AppRoutesValues } from '../const';
 
 export const redirectToRoute = createAction<AppRoutesValues>('redirectToRoute');
-
-export const setMovie = createAction<Movie>('movie/set');
-
-export const setPromoMovie = createAction<PromoMovie>('movie/setPromo');
-
-export const setReviews = createAction<Reviews>('reviews/set');
-
-export const setMyList = createAction<MoviePreviews>('movie/myList');
