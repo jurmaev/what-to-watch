6781c15bee14a2a5243667cf82956b84a8887dc9
jurmaev <smartemii03@gmail.com>
2023-11-28 20:02:55 +0500

    add mocks

---
+++ src/mocks/movie.ts
@@ -0,0 +1,20 @@
+export const mockMovie = {
+  id: '2cb8ff1d-0e12-4a0f-890c-a32e642ca92b',
+  name: 'Pulp Fiction',
+  posterImage:
+    'https://13.design.pages.academy/static/film/poster/Pulp_Fiction.jpg',
+  backgroundImage:
+    'https://13.design.pages.academy/static/film/background/Pulp_Fiction.jpg',
+  backgroundColor: '#795433',
+  videoLink: 'https://13.design.pages.academy/static/film/video/bike.mp4',
+  description:
+    'The lives of two mob hitmen, a boxer, a gangster & his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+  rating: 1.5,
+  scoresCount: 1635992,
+  director: 'Quentin Tarantino',
+  starring: ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson'],
+  runTime: 153,
+  genre: 'Crime',
+  released: 1994,
+  isFavorite: false,
+};
